3740941f5c005bdce4f823b8a513280050a94ef2
郎小姐 <langxue_dalang@163.com>
2023-08-07 16:30:54 +0800

    webpack配置

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,16 @@ const { name } = require("./package");
 module.exports = {
   lintOnSave: false,
   devServer: {
-    port: "8082",
+    port: "8088",
     headers: {
       'Access-Control-Allow-Origin': '*' // 主应用获取子应用时跨域响应头
+    },
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: true,
+      },
     },
   },
   configureWebpack: config => {
@@ -17,7 +24,9 @@ module.exports = {
       output: {
         library: `${name} - [name]`,
         libraryTarget: `umd`,
-        globalObject: "window"
+        globalObject: "window",
+        // chunkLoadingGlobal: `webpackJsonp_${name}` // 提供一个唯一的名称
+        // webpack4 jsonpFunction
       }
     }
   },
